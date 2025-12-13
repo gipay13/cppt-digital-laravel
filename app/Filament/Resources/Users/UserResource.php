3f991a9232ded91f41f1cc9918798742f1883dc3
Ghifary Anshar <ghifariansarariestiand@gmail.com>
2025-12-13 15:46:22 +0700

    feat: add user resource

---
+++ app/Filament/Resources/Users/UserResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources\Users;
+
+use App\Filament\Resources\Users\Pages\ManageUsers;
+use App\Models\User;
+use BackedEnum;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use UnitEnum;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUserCircle;
+
+    protected static ?string $label = 'Pengguna';
+
+    protected static string | UnitEnum | null $navigationGroup = 'Master Data';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Nama')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('password')
+                    ->label('Kata sandi')
+                    ->password()
+                    ->revealable(filament()->arePasswordsRevealable())
+                    ->required()
+                    ->rule(Password::default())
+                    ->showAllValidationMessages()
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                    ->same('passwordConfirmation')
+                    ->columnSpanFull(),
+                TextInput::make('passwordConfirmation')
+                    ->label('Konfirmasi kata sandi')
+                    ->password()
+                    ->revealable(filament()->arePasswordsRevealable())
+                    ->required()
+                    ->dehydrated(false)
+                    ->columnSpanFull()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('index')
+                    ->label('No.')
+                    ->rowIndex(),
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->searchable(),
+                ToggleColumn::make('is_active')
+                    ->label('Aktif'),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make()
+                    ->modalWidth('sm'),
+            ])
+            ->toolbarActions([
+                
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageUsers::route('/'),
+        ];
+    }
+}
