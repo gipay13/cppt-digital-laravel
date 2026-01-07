eec839f9d7305fd30a2f34fd5a14d8cad4e25b62
Ghifary Anshar <ghifariansarariestiand@gmail.com>
2026-01-07 20:43:18 +0700

    fix: change root route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Invokable\StreamCpptRecordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('filament.dashboard.auth.login');
 });
 
 Route::middleware(['auth', 'can:ViewPdf:Cppt,cppt'])->group(function () {
